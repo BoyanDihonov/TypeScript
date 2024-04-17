6eb8957a20079a826949fe63d940673954d0866c
Boyan Dihonov <149152266+BoyanDihonov@users.noreply.github.com>
2024-04-17 14:36:34 +0300

    Delete 07.biggerHalf.ts

--- 07.biggerHalf.ts
+++
@@ -1,9 +0,0 @@
-function biggestHalf(arr:number[]): number[] {
-    const sortedArray = arr.slice().sort((a, b) => a - b);
-    const middleIndex = Math.floor(sortedArray.length / 2);
-    return sortedArray.slice(middleIndex);
-
-}
-
-biggestHalf([4, 7, 2, 5])
-biggestHalf([3, 19, 14, 7, 2, 19, 6])
