de0849cebb591a26dc7dee082afc1a7631d24943
Boyan Dihonov <149152266+BoyanDihonov@users.noreply.github.com>
2024-04-17 14:35:59 +0300

    Delete 02.largestNumber.ts

--- 02.largestNumber.ts
+++
@@ -1,9 +0,0 @@
-function largestNumber(a: number, b: number, c: number) {
-
-    let number: number = Math.max(a, b, c)
-
-    console.log(`The largest number is ${number}.`)
-}
-
-largestNumber(5, -3, 16)
-largestNumber(-3, -5, -22.5)
